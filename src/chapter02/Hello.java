68315c6e52b0752f5697821de525ab215a7d97f4
Ha <a84656607@gmail.com>
2025-06-24 17:14:19 +0900

    .md file modifed commit

--- src/chapter02/Hello.java
+++ src/chapter02/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	//main method
 	public static void main(String[] args) {
 		System.out.println("welcome to JAVA!!");
-		System.out.println("hello");
+	    System.out.println("hello");
 	}
 	
 }
