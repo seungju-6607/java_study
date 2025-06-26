e47865f803f2889c2b5c40f6513d80986e0b64e4
Ha <a84656607@gmail.com>
2025-06-26 11:03:17 +0900

    .md file modifed commit

---
+++ src/chapter04/OpArithmeticTest.java
@@ -0,0 +1,21 @@
+package chapter04;
+
+/*
+ * 사칙 연산자 : +, -, *, /, %
+ */
+
+public class OpArithmeticTest {
+
+	public static void main(String[] args) {
+		int number1 = 5;
+		int number2 = 2;
+		
+		System.out.println("number1 + number2 = " + (number1 + number2));
+		System.out.println("number1 - number2 = " + (number1 - number2));
+		System.out.println("number1 * number2 = " + (number1 * number2));
+		System.out.println("number1 / number2 = " + (number1 / number2));
+		System.out.println("number1 % number2 = " + (number1 % number2));
+		
+	}
+
+}
---
+++ src/chapter04/OpComparisonTest.java
@@ -0,0 +1,14 @@
+package chapter04;
+
+/*
+ * 비교 연산자 : >, >=, <, <=, ==, !=
+ * 비교 연산자의 실행 결과는 boolean 타입으로 리턴되며, 제어문 if while 조건식에 사용됨
+ */
+public class OpComparisonTest {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
