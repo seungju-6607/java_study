8ca0d57dad1324677641e9540415dbac6a88d899
Ha <a84656607@gmail.com>
2025-06-30 16:28:25 +0900

    .md file modifed commit

--- src/chapter06/MultipleArrayTest02.java
+++ src/chapter06/MultipleArrayTest02.java
@@ -4,6 +4,20 @@ public class MultipleArrayTest02 {
 
 	public static void main(String[] args) {
 		// 가변형 배열 생성 : 3행의 구조에 열은 1부터 하나씩 증가하는 구조
+		int[][] matrix = new int [3][];	
+		matrix[0] = new int[1];
+		matrix[1] = new int[2];
+		matrix[2] = new int[3];
+		
+		int count = 10;
+		for(int i =0; i < matrix.length; i++) {
+			for(int j = 0; j <matrix[i].length; j++) {
+				matrix[i][j] = count;
+				System.out.print(matrix[i][j]);
+				count+=10;
+			}
+			System.out.println();
+		}
 
 	}
 
